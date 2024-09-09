906e273c61d9c2f1924cb4720c4cc5a3104c68bf
abdennour <abdennour@fluidefi.com>
2024-09-09 14:57:20 -0400

    llm service

---
+++ app/services/llm_factory.py
@@ -0,0 +1,98 @@
+from openai import AzureOpenAI
+from pydantic import BaseModel
+from typing import List, Dict, Type, Any
+from app.config import app_config
+
+
+class LlmFactory:
+    def __init__(self, provider: str):
+        self.provider = provider
+        self.client = self._initialize_client()
+
+    def _initialize_client(self) -> Any:
+        assert self.provider in ["azure-openai"]
+        if self.provider == "azure-openai":
+            return AzureOpenAI(
+                api_key=app_config.AZURE_OPENAI_API_KEY,
+                azure_endpoint=app_config.AZURE_OPENAI_ENDPOINT,
+                api_version="2024-08-01-preview",
+            )
+
+    def create_completion(
+        self,
+        model: str,
+        messages: List[Dict[str, str]],
+        response_format: Type[BaseModel] | None = None,
+        **kwargs,
+    ) -> Any:
+        completion_params = {
+            "model": model,
+            "temperature": kwargs.get("temperature", 0.2),
+            "messages": messages,
+        }
+        if response_format:
+            completion_params["response_format"] = response_format
+            return self.client.beta.chat.completions.parse(**completion_params)
+        return self.client.chat.completions.create(**completion_params)
+
+
+if __name__ == "__main__":
+    from app.models.schemas import FinancialStatement
+    from app.services.prompts import INVESTMENT_STATEMENT_DATA_EXTRACTION
+    from pydantic import Field
+
+    from app.utils.misc import output_to_excel
+    import time
+    from app.utils.utility import load_sample_statements_ocr
+
+    llm = LlmFactory("azure-openai")
+
+    class CompletionModel(BaseModel):
+        response: str = Field(description="Your response to the user.")
+        reasoning: str = Field(description="Explain your reasoning for the response.")
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "If it takes 2 hours to dry 1 shirt out in the sun, how long will it take to dry 5 shirts?",
+        },
+    ]
+
+    completion = llm.create_completion(
+        response_format=CompletionModel,
+        messages=messages,
+        model="gpt-4o",
+    )
+
+    print(completion.choices[0].message.parsed)
+
+    completion = llm.create_completion(messages=messages, model="gpt-4o-mini")
+    print(completion.choices[0].message)
+
+    sample_statements = load_sample_statements_ocr()
+    # sample_statement = sample_statements["bmo.pdf"]
+    sample_statement = open(
+        "./cleaned_texts/nbin_cleaned_text.txt", "r", encoding="utf-8"
+    ).read()
+    parse_start_time = time.time()
+    completion = llm.create_completion(
+        model="gpt-4o",
+        response_format=FinancialStatement,
+        messages=[
+            {
+                "role": "system",
+                "content": INVESTMENT_STATEMENT_DATA_EXTRACTION["system"],
+            },
+            {
+                "role": "user",
+                "content": INVESTMENT_STATEMENT_DATA_EXTRACTION["user"].format(
+                    statement_text=sample_statement
+                ),
+            },
+        ],
+    )
+    structured_data = completion.choices[0].message.parsed
+    output_to_excel(structured_data)
+    print(f"Selected item parsed in {time.time() - parse_start_time:.2f} seconds")
+    print(structured_data)
