fb3bf57071d20abc0e78994a8d5836ee60afa0cd
abdennour <abdennour@fluidefi.com>
2024-09-24 10:54:37 -0400

    User endpoint and db operations

---
+++ app/models/database/user_db.py
@@ -0,0 +1,128 @@
+from app.models.database.schema import User
+from app.models.schemas import UserBase
+from sqlalchemy.orm import Session
+from utils.hash import Hash
+from typing import List, Optional
+from sqlalchemy.exc import IntegrityError
+from pydantic import ValidationError
+from fastapi import HTTPException, status
+from app.models.enums import Role
+
+
+user_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="User not found",
+)
+
+
+def create_user(db: Session, request: UserBase) -> Optional[User]:
+    try:
+        new_user = User(
+            email=request.email,
+            firm_id=request.firm_id,
+            role=Role(request.role),
+            first_name=request.first_name,
+            last_name=request.last_name,
+            password=Hash.bcrypt(request.password),
+            phone_number=request.phone_number,
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists"
+        )
+
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Validation error: {e.errors()}",
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
+
+
+def get_all_users(db: Session) -> List[User]:
+    try:
+        # TODO: Only role of admin can get all users, for their own firm only
+        return db.query(User).all()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
+
+
+def get_user(db: Session, id: int) -> Optional[User]:
+    try:
+        user_data = db.query(User).filter(User.id == id).first()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
+
+    if user_data is None:
+        raise user_not_found_exception
+    return user_data
+
+
+def update_user(db: Session, id: int, request: UserBase) -> User:
+    user = get_user(db, id)
+    try:
+        user.email = request.email
+        if request.role == Role.PROSPECT.value:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Cannot update to role Prospect",
+            )
+        user.role = Role(request.role)
+        user.first_name = request.first_name
+        user.last_name = request.last_name
+        user.phone_number = request.phone_number
+        db.commit()
+        db.refresh(user)
+        return user
+
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already exists",
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
+
+
+def delete_user(db: Session, id: int) -> str:
+    user = get_user(db, id)
+    try:
+        db.delete(user)
+        db.commit()
+        return "ok"
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
+
+
+def get_all_users_by_firm(db: Session, firm_id: int) -> List[User]:
+    try:
+        return db.query(User).filter(User.firm_id == firm_id).all()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occurred: {str(e)}",
+        )
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends, status, HTTPException
 from utils.db_connection_manager import get_db
 from app.models.database import user_db
-from app.models.schemas import UserBase, UserDisplay
+from app.models.schemas import UserBase, UserDisplay, UserPut
 from sqlalchemy.orm import Session
 from utils.auth import get_current_user
 from app.models.enums import Role  # Import the Role enum
@@ -20,7 +20,12 @@ def check_admin_and_firm(current_user: UserBase, user_firm_id: int):
         raise forbidden_exception
 
 
-@router.post("/", response_model=UserDisplay)
+@router.post(
+    "/",
+    response_model=UserDisplay,
+    summary="Create a new user",
+    description="Create a new user in the database. This endpoint is accessible to all authenticated users.",
+)
 def create_user(
     request: UserBase,
     db: Session = Depends(get_db),
@@ -29,7 +34,12 @@ def create_user(
     return user_db.create_user(db, request)
 
 
-@router.get("/", response_model=List[UserDisplay])
+@router.get(
+    "/",
+    response_model=List[UserDisplay],
+    summary="Get all users",
+    description="Retrieve a list of all users. This endpoint is accessible only to users with the 'ADMIN' role.",
+)
 def get_all_users(
     db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)
 ):
@@ -41,7 +51,12 @@ def get_all_users(
     return users_list
 
 
-@router.get("/{id}", response_model=UserDisplay)
+@router.get(
+    "/{id}",
+    response_model=UserDisplay,
+    summary="Get a user by ID",
+    description="Retrieve a user by their ID. This endpoint is accessible only to users with the 'ADMIN' role.",
+)
 def get_user(
     id: int,
     db: Session = Depends(get_db),
@@ -52,10 +67,15 @@ def get_user(
     return user
 
 
-@router.put("/{id}", response_model=UserDisplay)
+@router.put(
+    "/{id}",
+    response_model=UserDisplay,
+    summary="Update a user",
+    description="Update a user's information. This endpoint is accessible only to users with the 'ADMIN' role.",
+)
 def update_user(
     id: int,
-    request: UserBase,
+    request: UserPut,
     db: Session = Depends(get_db),
     current_user: UserBase = Depends(get_current_user),
 ):
@@ -64,7 +84,12 @@ def update_user(
     return user_db.update_user(db, id, request)
 
 
-@router.delete("/{id}", response_model=UserDisplay)
+@router.delete(
+    "/{id}",
+    response_model=UserDisplay,
+    summary="Delete a user",
+    description="Delete a user by their ID. This endpoint is accessible only to users with the 'ADMIN' role.",
+)
 def delete(
     id: int,
     db: Session = Depends(get_db),
@@ -73,3 +98,17 @@ def delete(
     user = user_db.get_user(db, id)
     check_admin_and_firm(current_user, user.firm_id)
     return user_db.delete_user(db, id)
+
+
+@router.get(
+    "/roles",
+    response_model=List[str],
+    summary="Retrieve a list of roles",
+    description="Retrieve a list of roles. This endpoint is accessible only to users with the 'ADMIN' role.",
+)
+def get_roles(
+    current_user: UserBase = Depends(get_current_user),
+):
+    if current_user.role != Role.ADMIN:
+        raise forbidden_exception
+    return [role.name for role in Role if role != Role.PROSPECT]
