24d6f0e2d11174ba657ff7cfff84274d30e9f0c3
abdennour <abdennour@fluidefi.com>
2024-10-07 16:18:31 -0400

    added log prob

--- app/services/llm_factory.py
+++ app/services/llm_factory.py
@@ -19,7 +19,9 @@ class LlmFactory:
                 api_version="2024-08-01-preview",
             )
         elif self.provider == "local-lm-studio":
-            return OpenAI(base_url="http://127.0.0.1:1234/v1", api_key="not-needed")
+            return OpenAI(
+                base_url="http://127.0.0.1:1234/v1", api_key="not-needed"
+            )
 
     def create_completion(
         self,
@@ -32,6 +34,7 @@ class LlmFactory:
             "model": model,
             "temperature": kwargs.get("temperature", 0.2),
             "messages": messages,
+            "logprobs": True,
         }
         if response_format:
             completion_params["response_format"] = response_format
@@ -40,61 +43,40 @@ class LlmFactory:
 
 
 if __name__ == "__main__":
-    from app.models.schemas import FinancialStatement
-    from app.services.prompts import INVESTMENT_STATEMENT_DATA_EXTRACTION
     from pydantic import Field
-    import time
-    # from app.utils.utility import load_sample_statements_ocr
 
-    llm = LlmFactory("local-lm-studio")
+    llm = LlmFactory("azure-openai")
+    # llm = LlmFactory("local-lm-studio")
 
     class CompletionModel(BaseModel):
         response: str = Field(description="Your response to the user.")
-        reasoning: str = Field(description="Explain your reasoning for the response.")
+        reasoning: str = Field(
+            description="Explain your reasoning for the response."
+        )
 
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
+        # {
+        #     "role": "user",
+        #     "content": "If it takes 2 hours to dry 1 shirt out in the sun, how long will it take to dry 5 shirts?",
+        # },
+    ]
+    messages.append(
         {
             "role": "user",
-            "content": "If it takes 2 hours to dry 1 shirt out in the sun, how long will it take to dry 5 shirts?",
-        },
-    ]
-
-    completion = llm.create_completion(
-        response_format=CompletionModel,
-        messages=messages,
-        model="meta-llama-3.1-8b-instruct-q4_k_m",
+            "content": "What is the MER of PIMCO MONTHLY INCOME FUND SR A?",
+        }
     )
 
-    parsed_response = completion.choices[0].message.parsed
-    print(f"Parsed response: {parsed_response}")
-
-    completion = llm.create_completion(messages=messages, model="gpt-4o-mini")
+    completion = llm.create_completion(
+        # response_format=CompletionModel,
+        messages=messages,
+        model="gpt-4o",
+    )
     print(completion.choices[0].message)
 
-    # sample_statements = load_sample_statements_ocr()
-    # sample_statement = sample_statements["bmo.pdf"]
-    sample_statement = open(
-        "./cleaned_texts/nbin_cleaned_text.txt", "r", encoding="utf-8"
-    ).read()
-    parse_start_time = time.time()
-    completion = llm.create_completion(
-        model="gpt-4o",
-        response_format=FinancialStatement,
-        messages=[
-            {
-                "role": "system",
-                "content": INVESTMENT_STATEMENT_DATA_EXTRACTION["system"],
-            },
-            {
-                "role": "user",
-                "content": INVESTMENT_STATEMENT_DATA_EXTRACTION["user"].format(
-                    statement_text=sample_statement
-                ),
-            },
-        ],
-    )
-    structured_data = completion.choices[0].message.parsed
-    # output_to_excel(structured_data)
-    print(f"Selected item parsed in {time.time() - parse_start_time:.2f} seconds")
-    print(structured_data)
+    # parsed_response = completion.choices[0].message.parsed
+    # print(f"Parsed response: {parsed_response}")
+
+    # completion = llm.create_completion(messages=messages, model="gpt-4o-mini")
+    # print(completion.choices[0].message)
