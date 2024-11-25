5323ea309a97b5005267f2694ecdb2ac50324c76
abdennour <abdennour@fluidefi.com>
2024-11-25 10:42:37 -0500

    Add client email and name

--- app/models/schemas/auth_schema.py
+++ app/models/schemas/auth_schema.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel, Field
 
 
 class TokenResponseSchema(BaseModel):
-    access_token: str = Field(..., description="The access token")
-    token_type: str = Field(..., description="The type of the token")
-    expires_at: int = Field(..., description="The expiration time of the access token as a Unix epoch timestamp")
+    access_token: str = Field(..., description="JWT access token")
+    token_type: str = Field(..., description="Token type (bearer)")
+    expires_at: int = Field(..., description="Token expiration timestamp")
+    email: str = Field(..., description="User's email address")
+    first_name: str = Field(..., description="User's first name")
+    last_name: str = Field(..., description="User's last name")
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter, Depends, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.models.schemas.auth_schema import TokenResponseSchema
-from app.utils.auth import authenticate_user, create_access_token, create_refresh_token, verify_token, get_user
+from app.utils.auth import (
+    authenticate_user,
+    create_access_token,
+    create_refresh_token,
+    verify_token,
+    get_user,
+)
 from app.utils.db_connection_manager import get_db
 from fastapi import Cookie
 from app.config import app_config
@@ -17,20 +23,25 @@ def login_for_access_token(
     db: Session = Depends(get_db),
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
-    access_token, access_expires = create_access_token(user.email, str(user.id), user.role.value)
+    access_token, access_expires = create_access_token(
+        user.email, str(user.id), user.role.value
+    )
     refresh_token, refresh_expires = create_refresh_token(str(user.id))
     response.set_cookie(
         key="refresh_token",
         value=refresh_token,
         httponly=True,
         secure=True,
-        samesite='none' if app_config.DEBUG else 'lax',
-        expires=refresh_expires
+        samesite="none" if app_config.DEBUG else "lax",
+        expires=refresh_expires,
     )
     return {
         "access_token": access_token,
         "token_type": "bearer",
-        "expires_at": access_expires
+        "expires_at": access_expires,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
     }
 
 
@@ -38,22 +49,24 @@ def login_for_access_token(
 async def refresh_access_token(
     response: Response,
     refresh_token: str = Cookie(None),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ):
     user_id = verify_token(refresh_token, "refresh")
     user = get_user(db, int(user_id))
-    access_token, access_expires = create_access_token(user.email, str(user.id), user.role.value)
+    access_token, access_expires = create_access_token(
+        user.email, str(user.id), user.role.value
+    )
     new_refresh_token, refresh_expires = create_refresh_token(str(user.id))
     response.set_cookie(
         key="refresh_token",
         value=new_refresh_token,
         httponly=True,
         secure=True,
-        samesite='none' if app_config.DEBUG else 'lax',
-        expires=refresh_expires
+        samesite="none" if app_config.DEBUG else "lax",
+        expires=refresh_expires,
     )
     return {
         "access_token": access_token,
         "token_type": "bearer",
-        "expires_at": access_expires
+        "expires_at": access_expires,
     }
